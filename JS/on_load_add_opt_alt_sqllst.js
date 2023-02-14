aed17c61afe6c6ec5e920658cc15701020a0bfd0
ADRIAN NICOLAE BRICIU <adrian.briciu83@gmail.com>
2023-02-14 15:57:29 +0200

    upd_selec_init_art_inreg_sqltbl

--- JS/on_load_add_opt_alt_sqllst.js
+++ JS/on_load_add_opt_alt_sqllst.js
@@ -1,6 +1,6 @@
 function on_load_add_opt_alt_sqllst() {
-    var option_tip_art = 'Altul';
-    $(sqllst_tip_art).append(new Option(option_tip_art));
+    var option_tip_articol = 'Altul';
+    $(sqllst_tip_articol).append(new Option(option_tip_articol));
     var option_categ_si_subcateg = 'Alta';
     $(sqllst_categorie).append(new Option(option_categ_si_subcateg));
     $(sqllst_subcategorie).append(new Option(option_categ_si_subcateg));
